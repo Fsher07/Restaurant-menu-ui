f3be3b47da9bc0caf823773a1e34518f9dffafd5
Solomon Achilles <solohag92@gmail.com>
2022-04-13 09:19:48 +0000

    Update style.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import populateMeals from '../modules/add.js';
-import { getLikes } from '../modules/api2.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   populateMeals();
-  getLikes();
 });
