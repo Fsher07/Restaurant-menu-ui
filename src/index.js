972d64184e2b622ff0cbda5c5bc022e3513a242a
Fsher07 <furkansalimm@gmail.com>
2022-04-14 18:44:59 +0300

    Uptade style.css and index.js
    -Added overflow:hidden to body when comment window popup

--- src/index.js
+++ src/index.js
@@ -13,10 +13,12 @@ document.addEventListener('click', (e) => {
     popupComments(e.target.parentElement.id);
     displayComments(e.target.parentElement.id);
     document.getElementsByTagName('main')[0].style.filter = 'blur(4px)';
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
   }
   if (e.target.className === 'fa-solid fa-x') {
     closeCommentWindow();
     document.getElementsByTagName('main')[0].style.filter = 'none';
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
   }
 });
 
